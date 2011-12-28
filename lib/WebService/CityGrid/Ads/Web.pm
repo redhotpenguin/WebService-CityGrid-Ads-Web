18c15376af7bb9af9a6d66a8324c7dc05f4071b3
Fred Moyer <fred@redhotpenguin.com>
2011-12-28 00:30:31 -0800

    license

--- lib/WebService/CityGrid/Ads/Web.pm
+++ lib/WebService/CityGrid/Ads/Web.pm
@@ -80,16 +80,11 @@ WebService-CityGrid-Ads-Web - CityGrid web ads interface
 
 =head1 SYNOPSIS
 
-  use WebService-CityGrid-Ads-Web;
-  blah blah blah
+  use WebService::CityGrid::Ads::Web;
 
 =head1 DESCRIPTION
 
-Stub documentation for WebService-CityGrid-Ads-Web, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+Alpha API into CityGrid web ads
 
 =head2 EXPORT
 
@@ -97,24 +92,13 @@ None by default.
 
 
 
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
 =head1 AUTHOR
 
-Fred Moyer, E<lt>phred@redhotpenguin.comE<gt>
+Fred Moyer, E<lt>fred@slwifi.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 by Fred Moyer
+Copyright (C) 2011 Silver Lining Networks
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.1 or,
